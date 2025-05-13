4d33999db01521f38440a799161aa6c7b0b6e4b6
Nacuy <y969087887@gmail.com>
2025-05-13 19:36:52 +0100

    add new icons and launch-able on iOS

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,51 @@ import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "apple-touch-icon.png", "icons/icon-192.png", "icons/icon-512.png"],
+      manifest: {
+        name: "QXOTE Input App",
+        short_name: "Input App",
+        description: "QXOTE & Technova input app",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0d6efd",
+        icons: [
+          {
+            src: "icons/icon-192.jpg",
+            sizes: "192x192",
+            type: "image/jpg",
+          },
+          {
+            src: "icons/icon-512.jpg",
+            sizes: "512x512",
+            type: "image/jpg",
+          },
+          {
+            src: "app_logo.jpg",
+            sizes: "180x180",
+            type: "image/jpg",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true,
   },
 });
