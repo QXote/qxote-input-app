ccf6f2e39eda9072f7a9bd66e6d44c01ca147e78
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-05-13 15:32:37 +0100

    Installed ShadCN

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
