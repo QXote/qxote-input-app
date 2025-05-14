10620c9b4b3439838b2facb2ccc8738ee18d6487
Nacuy <y969087887@gmail.com>
2025-05-14 17:42:56 +0100

    data table with demo data

---
+++ src/models/plantDTO.ts
@@ -0,0 +1,10 @@
+export interface plantDTO {
+    plantNr: string;
+    coordinate: string;
+    species: string;
+    cover: string;
+    temperature: string;
+    humidity: string;
+    date: string;
+    zone: string;
+}
