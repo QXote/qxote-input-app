ce45baf5edd984f1e734bed14455c6131632df39
SanderFrakking <s.d.frakking@gmail.com>
2025-05-14 17:28:06 +0200

    branch init

--- src/components/features/MultiStepForm/StepOne.tsx
+++ src/components/features/MultiStepForm/StepOne.tsx
@@ -3,10 +3,11 @@ import {Button} from "@/components/ui/button.tsx";
 
 function SelectPlotPage() {
     return <div className="flex flex-col items-center justify-center min-h-screen space-y-2">
+        <h1 className="font-bold text-3xl md:text-2xl">Plot kiezen </h1>
         <div>
             <img src="/app_logo.jpg" className="logo" alt="app logo" />
         </div>
-        <h1 className="text-2xl font-bold">Select Plot</h1>
+        <p>selecteer het plot </p>
         <Combobox />
         <Button>Next Step</Button>
     </div>
